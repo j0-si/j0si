e97637b76c7e94b0600a9d63d168a7bbb3cad66d
Driplase <258vr.g.subspeed@gmail.com>
2024-10-20 21:27:01 +0900

    probably all done i guess

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 /*
     serviceURL = `${PROTOCOL}//${HOSTNAME}/`
 */
-const serviceURL = "http://j0.si/", port = 1071
 
 function grs(length) {
     const b = "0123456789qwertyuiopasdfghjklzxcvbnm-_"
@@ -12,83 +11,130 @@ function grs(length) {
     return y;
 }
 
-const express = require('express'), app = express(), path = require('node:path'), fs = require('node:fs')
+const express = require('express'), app = express(), path = require('node:path'), fs = require('node:fs'), { rateLimit } = require('express-rate-limit')
+const config = {}
+fs.readFileSync('./config').toString().replace(/\r/g, "").split('\n').forEach(i=>{let I=i.split('=');config[I[0]]=/(true)|(false)/.test(I[1])?I[1]==='true':I[1]})
+console.log(config)
+const serviceURL = config['url'], port = parseInt(config['port'])
+const t = rateLimit({
+    windowMs: 1e3,
+    limit: parseInt(config['rate-limit']),
+    standardHeaders: 'draft-7',
+    legacyHeaders: false,
+    handler: (req, res) => {
+        res.status(429).send(ec(9))}
+})
+app.use(t)
 app.use(express.urlencoded({ extended: true }));
-app.set('trust proxy', true);
+app.set('trust proxy', 1)
 app.post('/shorten', (req, res) => {
-    console.log(`${new Date()} // ${req.ip}`)
-    console.log(req.query);
-    if (req.query.url) {
-        if (/[\+]+/.test(req.query.path)) {
-            res.status(400).send(ec(2))
-        } else {
-            let p, x, y = false;
-            const d = JSON.parse(fs.readFileSync('./links.json')), pd = d.map(i => i.path.replace(/^\//, ''))
-            if (req.query.path) {
-                p = encodeURI(req.query.path.replace(/^\s+/, '').replace(/\s+$/, ''));
+    try {
+        console.log(`${new Date()} // ${req.ip}`)
+        console.log(req.query);
+        if (req.query.url) {
+            if (/[\+]+/.test(req.query.path)) {
+                res.status(400).send(ec(2))
             } else {
-                let k = 0, l = 4;
-                y = true;
-                do {
-                    p = grs(l)
-                    k++;
-                    if (k % 100 === 0) {
-                        l++;
-                    }
-                } while (pd.includes(p))
-            }
-            if (p.length < 3) {
-                res.status(403).send(ec(4))
-            } else {
-                if (pd.includes(p)) {
-                    res.status(403).send(ec(5))
+                let p, y = false;
+                const d = JSON.parse(fs.readFileSync('./links.json')), pd = d.map(i => i.path.replace(/^\//, ''))
+                if (req.query.path) {
+                    p = encodeURI(req.query.path.replace(/^\s+/, '').replace(/\s+$/, ''));
                 } else {
-                    const j = `${serviceURL}${p}`, u = encodeURI(req.query.url);
-                    if (j.length < u.length || !y) {
-                        x = {
-                            url: u,
-                            path: p
+                    let k = 0, l = 4;
+                    y = true;
+                    do {
+                        p = grs(l)
+                        k++;
+                        if (k % 100 === 0) {
+                            l++;
                         }
-                        d.push(x)
-                        x.c = "OK", x.i = 0
-                        fs.writeFileSync('./links.json', JSON.stringify(d, null, 2))
-                        res.status(201).send(x)
+                    } while (pd.includes(p))
+                }
+                if (p.length < 3) {
+                    res.status(403).send(ec(4))
+                } else {
+                    if (pd.includes(p)) {
+                        res.status(403).send(ec(5))
                     } else {
-                        res.status(403).send(ec(6))
+                        const j = `${serviceURL}${p}`, u = encodeURI(req.query.url);
+                        if ((j.length < u.length || !y) && config['only-shortened']) {
+                            const r = new URL(u)
+                            r.protocol = new URL(serviceURL).protocol
+                            if (j == r.href && !config['allow-redirect-loop']) {
+                                res.status(403).send(ec(7))
+                            } else {
+                                const f = JSON.parse(fs.readFileSync('./links.json')), b = JSON.parse(fs.readFileSync('./blacklist.json'))
+                                let q = false;
+                                if (`${r.origin}/` === serviceURL) {
+                                    const h = f.find(i => `/${i.path}` === r.pathname)
+                                    if (h) {
+                                        q = true
+                                    }
+                                }
+                                if (q && !config['allow-redirect-loop']) {
+                                    res.status(403).send(ec(7))
+                                } else {
+                                    d.push({
+                                        url: u,
+                                        path: p
+                                    })
+                                    fs.writeFileSync('./links.json', JSON.stringify(d, null, 2))
+                                    updateStats(0)
+                                    res.status(201).send({
+                                        url: u,
+                                        path: p,
+                                        c: "OK",
+                                        i: 0
+                                    })
+                                }
+                            }
+                        } else {
+                            res.status(403).send(ec(6))
+                        }
                     }
                 }
             }
+        } else {
+            res.status(400).send(ec(1))
         }
-    } else {
-        res.status(400).send(ec(1))
+    } catch (e) {
+        console.error(e)
     }
 })
 
 app.get('/*', (req, res) => {
-    const u = req.url;
-    if (/^\/(index.html)?$/.test(u)) {
-        res.status(200).sendFile(path.join(__dirname, 'htdocs', 'index.html'))
-    } else if (/^\/.+/.test(u)) {
-        const d = JSON.parse(fs.readFileSync('./links.json')), pd = d.map(i => i.path.replace(/^\//, '')), p = u.replace(/^\//, '').replace(/\+$/, '');
-        if (pd.includes(p)) {
-            if (/\+$/.test(u)) {
-                res.status(200).send({
-                    url: d[pd.findIndex(i => i === p)].url,
-                    path: p,
-                    c: "OK",
-                    i: 0
-                })
-            } else {
-                res.redirect(301, d[pd.findIndex(i => i === p)].url)
-            }
-            
+    try {
+        const u = req.url;
+        if (u === "/stats.json") {
+            res.status(200).sendFile(path.join(__dirname, 'stats.json'))
         } else {
-            if (/\+$/.test(u)) {
-                res.status(404).send(ec(3))
-            } else {
-                res.status(404).sendFile(path.join(__dirname, 'htdocs', '404.html'))
+            if (/^\/(index.html)?$/.test(u)) {
+                res.status(200).sendFile(path.join(__dirname, 'htdocs', 'index.html'))
+            } else if (/^\/.+/.test(u)) {
+                const d = JSON.parse(fs.readFileSync('./links.json')), pd = d.map(i => i.path.replace(/^\//, '')), p = u.replace(/^\//, '').replace(/\+$/, '');
+                if (pd.includes(p)) {
+                    if (/\+$/.test(u)) {
+                        res.status(200).send({
+                            url: d[pd.findIndex(i => i === p)].url,
+                            path: p,
+                            c: "OK",
+                            i: 0
+                        })
+                    } else {
+                        updateStats(1)
+                        res.redirect(307, d[pd.findIndex(i => i === p)].url)
+                    }
+                } else {
+                    if (/\+$/.test(u)) {
+                        res.status(404).send(ec(3))
+                    } else {
+                        res.status(404).sendFile(path.join(__dirname, 'htdocs', '404.html'))
+                    }
+                }
             }
         }
+    } catch (e) {
+        console.error(e)
     }
 })
 
@@ -101,5 +147,19 @@ function ec(num) {
     return {
         c: e[num] ?? `x${num}`,
         i: num
+    }
+}
+
+function updateStats(a) {
+    try {
+        const k = JSON.parse(fs.readFileSync('./stats.json')), o = JSON.parse(fs.readFileSync('./links.json'))
+        console.log(k)
+        k.g = o.length
+        if (a) {
+            k.a++
+        }
+        fs.writeFileSync('./stats.json', JSON.stringify(k, null, 2))
+    } catch (e) {
+        console.error(e)
     }
 }
--- migrateFromPHP.js
+++ migrateFromPHP.js
@@ -1,13 +1,31 @@
-const from = 'C:/Users/258vr/Downloads/j0si241023-main', to = './links.json';
-const fs = require('node:fs'), l = fs.readdirSync(from).filter(i => !/\.\S+/.test(i)), g = JSON.parse(fs.readFileSync(to))
-for (let j in l) {
-    try {
-        g.push({
-            url: fs.readFileSync(`${from}/${l[j]}/index.html`).toString().match(/content="https?:\/\/[^"]+"/g)[0].slice(9, -1),
-            path: l[j]
-        })
-    } catch (e) {
-        console.error(e)
+let from, to = './links.json';
+const fs = require('node:fs'), readline = require('node:readline'), r = readline.createInterface({ input: process.stdin, output: process.stdout})
+r.question('import links from: ', i => {
+    from = i.replace(/\\/g, '/').replace(/^".+"$/, r => { return r.slice(1, -1) });
+    const l = fs.readdirSync(from).filter(i => !/\.\S+/.test(i)), g = JSON.parse(fs.readFileSync(to)), m = g.map(i => i.path)
+    let s = 0, f = 0, u = 0
+    for (let j in l) {
+        try {
+            console.log(encodeURI(l[j]) in m)
+            if (m.includes(encodeURI(l[j]))) {
+                const w = g.find(i => i.path === encodeURI(l[j]))
+                w.url = fs.readFileSync(`${from}/${l[j]}/index.html`).toString().match(/content="https?:\/\/[^"]+"/g)[0].slice(9, -1)
+                console.log(`Updated url: ${l[j]}`)
+                u++
+            } else {
+                g.push({
+                    url: fs.readFileSync(`${from}/${l[j]}/index.html`).toString().match(/content="https?:\/\/[^"]+"/g)[0].slice(9, -1),
+                    path: encodeURI(l[j])
+                })
+                console.log(`Added url: ${l[j]}`)
+                s++
+            }
+        } catch (e) {
+            console.error(e)
+            f++
+        }
     }
-}
-fs.writeFileSync(to, JSON.stringify(g, null, 2))
+    fs.writeFileSync(to, JSON.stringify(g, null, 2))
+    console.log(`${s} added, ${u} updated, ${f} failed`)
+    r.close()
+})
